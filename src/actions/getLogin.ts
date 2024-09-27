aab9b549a7f808b41274b58a12bc95c42febd3de
Elobb Technologies <anisulhoque587@gmail.com>
2024-09-27 20:34:04 +0600

    issu fixed

--- src/actions/getLogin.ts
+++ src/actions/getLogin.ts
@@ -3,10 +3,17 @@
 import { signIn, signOut } from "../auth";
 
 export const getAuthLogin = async (formData: FormData) => {
-  const action = formData.get("action");
+  const action = formData.get("action") as string | null;
 
-  await signIn(action, { redirect: "/profile" });
+  if (action) {
+    // Only call signIn if action is not null
+    await signIn(action, { redirect: "/profile" });
+  } else {
+    // Handle case where action is null, if necessary
+    throw new Error("Action cannot be null.");
+  }
 };
+
 export const getAuthLogout = async () => {
   await signOut();
 };
